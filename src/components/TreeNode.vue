a31553fa80f6e27763f95168a9a1914140f11900
hjl <2822195657@qq.com>
2023-05-04 17:08:29 +0800

    on tree add delete and part of insert

--- src/components/TreeNode.vue
+++ src/components/TreeNode.vue
@@ -1,9 +1,18 @@
 <template>
   <div v-if="node" @contextmenu="showPopup">
     <div v-show="showMenu" :style="{ top: popupTop + 'px', left: popupLeft + 'px' }" class="popup">
-      <p class="popup-item">添加</p>
-      <p class="popup-item">删除</p>
+      <p class="popup-item" v-if="node.level<3" @click="showUpdate">添加</p>
+      <p class="popup-item" @click="deleteNode">删除</p>
       <p class="popup-item">修改</p>
+    </div>
+    <div v-show="showUpdateMenu" :style="{ top: popupTop + 'px', left: popupLeft + 'px' }" class="update-menu">
+      <div v-if="node.level==1">
+        <label for="sitename" class="update-menu-item">站点名称:</label>
+        <input type="text" id="sitename" ref="siteName" name="sitename" >
+        <label for="sitestate" class="update-menu-item">站点状态:</label>
+        <input type="text" id="sitestate" ref="siteState" name="sitestate" >
+        <input type="button" value="提交" class="pushbutton" @click="submit">
+      </div>
     </div>
     <div class="node-tree" 
       :style="{ 'padding-left': (node.level - 1) * 0.5 + 'rem' }"
@@ -33,7 +42,9 @@
 </template>
 
 <script>
-  import {addClass, removeClass} from "./model/dom"  
+  import axios from 'axios';
+  import {addClass, removeClass} from "./model/dom"
+    
   export default {
     name:"TreeNode",
     components: {
@@ -43,6 +54,7 @@
       return{
         expanded:true,
         showMenu: false,
+        showUpdateMenu: false,
         popupTop: 0, // 弹窗距离文档顶部的距离
         popupLeft: 0 // 弹窗距离文档左侧的距离
       }
@@ -137,6 +149,45 @@
       hidePopup() {
         // 隐藏弹窗
         this.showMenu = false;
+      },
+      showUpdate(event) {
+        // 计算弹窗位置
+        this.popupTop = event.pageY;
+        this.popupLeft = event.pageX;
+        // 在下一个周期显示弹窗，本周期将弹窗隐藏解决多次连续右击出现多个弹窗的情况
+        setTimeout(() => {
+          this.showUpdateMenu = true
+        }, 0);
+      },
+      hideUpdate() {
+        // 隐藏弹窗
+        this.showUpdateMenu = false;
+      },
+      deleteNode(){
+        let level = this.node.level;
+        let id = this.node.data.id;
+        axios.get(`http://localhost:8080/deleteByLevelAndId?level=${level}&id=${id}`)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+      },
+      submit(){
+        if(this.node.level==1){
+          let name = this.$refs.siteName.value;
+          let state = this.$refs.siteState.value;
+          let belong = this.node.data.id;
+          this.hideUpdate();
+          axios.get(`http://localhost:8080/insertSite?name=${name}&state=${state}&belong=${belong}`)
+          .then(response => {
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.log(error);
+          })
+        }
       }
     },
     watch:{
@@ -152,10 +203,12 @@
     mounted() {
       document.addEventListener('click', this.hidePopup);
       document.addEventListener('contextmenu', this.hidePopup);
+      document.addEventListener('contextmenu', this.hideUpdate);
     },
     beforeUnmount() {
       document.removeEventListener('click', this.hidePopup);
       document.addEventListener('contextmenu', this.hidePopup);
+      document.removeEventListener('contextmenu', this.hideUpdate);
     },
   }
 </script>
@@ -195,5 +248,47 @@
   }
   .popup-item:hover {
     background-color: #f5f5f5;
+  }
+  .update-menu {
+    position: absolute;
+    z-index: 9999;
+    background-color: #fff;
+    border: 0.05rem solid #ccc;
+    border-radius: 0.3rem;
+    box-shadow: 0 0.1rem 0.3rem rgba(0, 0, 0, 0.3);
+    padding: 0.1rem 0;
+    width: 6rem;
+
+  }
+  .update-menu-item {
+    display: inline;
+    padding: 0.1rem 0.2rem;
+    margin: 0.1rem;
+    font-size: 0.2rem;
+    color: #333;
+    text-decoration: none;
+    cursor: pointer;
+    height: 0.4rem;
+    line-height: 0.4rem;
+  }
+  .update-menu-item:hover {
+    background-color: #f5f5f5;
+  }
+  input{
+    display: inline;
+    width: 2.5rem;
+    padding: 0.1rem 0.2rem;
+    margin: 0.1rem;
+    font-size: 0.2rem;
+    color: #333;
+    text-decoration: none;
+    height: 0.4rem;
+    line-height: 0.4rem;
+  }
+  .pushbutton{
+    width: 1.5rem;
+    height: 1rem;
+    font-size: 0.2rem;
+    margin: 0 2.2rem 0 2.2rem;
   }
 </style>
